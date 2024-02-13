ca2225566d0df53a9c2656577f9d239f2d7b981d
ethior0 <thiagomeigmoreira18@gmail.com>
2024-02-12 22:27:53 -0300

    novas aulas 12-02

---
+++ Sec 02/aula6/index.js
@@ -0,0 +1,14 @@
+let nome; // Declarou a variável
+nome = "João"; // Inicializando a variável
+console.log(nome);
+nome = "Otávio";
+console.log(nome);
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive - nomeCliente != nomecliente
+// Não podemos redeclarar variáveis com let
+// NÃO UTILIZE VAR, UTILIZE LET.
---
+++ Sec 02/tests.js
@@ -0,0 +1,7 @@
+let nome = "Otávio";
+
+console.log(nome, "nasceu em 1984.");
+console.log("Em 2000", nome, "conheceu Maria.");
+console.log(nome, "casou-se com Maria em 2012.");
+console.log("Maria teve 1 filho com", nome, "em 2015.");
+console.log("O filho de", nome, "se chama Eduardo.");
